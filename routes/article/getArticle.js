30087ac6f47b2a6847cd0995ca48a002980f540f
leejongho <zhtmah1@naver.com>
2019-11-15 00:24:05 +0900

    getArticle - working

--- routes/article/getArticle.js
+++ routes/article/getArticle.js
@@ -4,18 +4,19 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/article',function(req,res,next){
-    var reqId = req.query.ID;
-    var reqFirstDate = req.query.FIRST_DATE;
-    var reqLastDate = req.query.LAST_DATE;
-    console.log(reqId);
-    console.log(reqFirstDate);
-    console.log(reqLastDate);
-    var queryString = `SELECT * FROM PRIVATE_ARTICLE WHERE ID = '${reqId}' AND CREATED_DATE BETWEEN '${reqFirstDate}' AND '${reqLastDate}'`;
-    console.log(queryString)
-    connection.query(queryString,function(err,result){
-        console.log(result);
-        res.send(result);
+    var userId = req.query.userId;
+    var friendsJson = req.query.friends;
+    var friends = JSON.parse(friendsJson);
+    var stringFriendId = `'${userId}'`;
+    friends.map((item)=>{
+        stringFriendId = stringFriendId + `, '${item.FRIEND_ID}'`
     })
+    var queryString = `SELECT * FROM PRIVATE_ARTICLE WHERE ID = '${reqId}' AND CREATED_DATE BETWEEN '${reqFirstDate}' AND '${reqLastDate}'`;
+    // console.log(queryString)
+    // connection.query(queryString,function(err,result){
+    //     console.log(result);
+    //     res.send(result);
+    // })
 })
 
 module.exports = router;
