175a1f6f48d15d58e12364a4e2a7d7eed3b45ef3
hokeys <substantiation@naver.com>
2019-12-04 12:58:02 +0900

    admin function -working

--- routes/group/group.js
+++ routes/group/group.js
@@ -9,9 +9,11 @@ router.post('/group/join',function(req,res,next){
     var groupKey = req.body.GROUP_KEY;
     var memberRowId = uuid.v1();
 
-    var queryString = `INSERT INTO group_members VALUES('${memberRowId}','${groupKey}','${user}',0)`;
+    var queryString = `INSERT INTO group_members VALUES('${memberRowId}','${groupKey}','${user}',0,1)`;
     connection.query(queryString, function (err, result) {
         connection.query(`SELECT * FROM group_members WHERE MEMBER_ROW_ID = '${memberRowId}'`,function(err,memberRow){
+            console.log(err);
+            console.log(memberRow);
             res.send(memberRow)
         })
     })
@@ -24,6 +26,29 @@ router.put('/group/join',function(req,res,next){
     connection.query(queryString,function(err,result){
         console.log(err);
         console.log(result);
+        res.send(result);
+    })
+})
+
+router.get('/group/members',function(req,res,next){
+    var searchMemberKeword = req.query.searchMemberKeword;
+    var groupKey = req.query.groupKey;
+    console.log(groupKey)
+    console.log(searchMemberKeword)
+    var queryString = `SELECT member_id,member_row_id,level from group_members where group_key = '${groupKey}' AND member_id like '%${searchMemberKeword}%' AND group_members.check != 0`;
+    connection.query(queryString,function(err,result){
+        console.log(err);
+        console.log(result);
+        res.send(result);
+    })
+})
+
+
+router.put('/group/members',function(req,res,next){
+    var member_row_id= req.data.memberRowId;
+    var level = req.data.level;
+    var queryString = `update group_members set level = ${level} where member_row_id = '${member_row_id}'`;
+    connection.query(queryString,function(err,result){
         res.send(result);
     })
 })
